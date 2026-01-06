ed6840d5a17a20f0b339df37c8135c624f95c71e
AymericGaudin <ay.gaudin@gmail.com>
2026-01-06 10:54:53 +0100

    Small correction to recipe generation

--- containers/generate_images.py
+++ containers/generate_images.py
@@ -15,7 +15,7 @@ from datetime import date
 
 
 def main(working_dir):
-    """ Parse available Docker files and generates associated images.
+    """ Parse available Docker files and generates associated recipes.
 
     Parameters
     ----------
@@ -28,22 +28,30 @@ def main(working_dir):
     for path in glob.glob(os.path.join(image_dir, "Dockerfile.*")):
         basename = os.path.basename(path)
         name = basename.split(".", 1)[1]
+        # create destination directory
         dest_dir = os.path.join(working_dir, name)
         if not os.path.isdir(dest_dir):
             os.mkdir(dest_dir)
+        # add tmp and cache dir
         tmp_dir = os.path.join(dest_dir, "tmp")
         cache_dir = os.path.join(dest_dir, "cache")
         for _path in (tmp_dir, cache_dir):
             if not os.path.isdir(_path):
                 os.mkdir(_path)
+        # copy the files in brainprep/containers/resources
+        resources = os.path.join(os.path.dirname(path), "resources", "*")
+        for resource_file in glob.glob(resources):
+            shutil.copy(resource_file, dest_dir)
+        # copy the appropriate Dockerfile
         shutil.copy(path, os.path.join(dest_dir, "Dockerfile"))
+        # create the commands list to run
         cmds = "export WDIR={}\n".format(dest_dir)
         cmds += "cd $WDIR\n"
         cmds += "export IMG={}\n".format(name)
         cmds += "sudo docker build --no-cache --tag brainprep-$IMG .\n"
         cmds += "sudo docker images\n"
         cmds += ("sudo docker save -o brainprep-$IMG-{0}.tar "
-                 "brainprep-$IMG:{0}\n".format(today))
+                 "brainprep-$IMG:latest\n".format(today))
         cmds += "sudo chmod 755 brainprep-$IMG-{}.tar\n".format(today)
         cmds += ("sudo SINGULARITY_TMPDIR=$WDIR/tmp SINGULARITY_CACHEDIR="
                  "$WDIR/cache singularity build brainprep-$IMG-{}.simg "
